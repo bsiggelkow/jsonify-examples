f2638c1bea8a82838a973dd70a9c668eeacccacc
Bill Siggelkow <bsiggelkow@mac.com>
2011-08-05 22:33:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsonifyExamples::Application.config.secret_token = '00f4032a940e1f8ded193023ee86b43b04105af5a11cf0a413b2c61d85002f4a0ae9cf5783875464457bc1e183695f9f5db6de1dbfe8ee2e665a0e67e7a52efa'
