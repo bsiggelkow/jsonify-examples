f3527e8782803c27ff2058bbeab93b11a47609f9
Bill Siggelkow <bsiggelkow@mac.com>
2011-09-08 21:38:56 -0400

    Changed naughty message

--- app/controllers/try_controller.rb
+++ app/controllers/try_controller.rb
@@ -6,7 +6,12 @@ class TryController < ApplicationController
   
   def eval_jsonify
     e = Evaluator.new params[:try_source]
-    result = 'Stop being naughty!'
+    result = <<-NAUGHTY
+    There are Ten Million Million Million Million
+    Particles in the Universe That We Can Observe
+    Your Momma Took the Ugly Ones
+    And Put Them into One Nerd
+    NAUGHTY
     t = Thread.new {result = e.evaluate}
     status = t.join(1)
     t.kill unless status
